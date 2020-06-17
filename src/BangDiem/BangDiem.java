0f0a03cca474feeb9afefd8ed915d05a00d2a195
haminhtoan123 <haminhtoan123@users.noreply.github.com>
2020-06-17 12:04:28 +0700

    add BangDiem

---
+++ src/BangDiem/BangDiem.java
@@ -0,0 +1,13 @@
+package BangDiem;
+
+public class BangDiem {
+	private String mssv;
+	private String hoten;
+	private String tenlop;
+	private String mamon;
+	private float diemgk;
+	private float diemck;
+	private float diemkhac;
+	private float diemtong;
+	
+}
---
+++ src/BangDiem/BangDiemDAO.java
@@ -0,0 +1,5 @@
+package BangDiem;
+
+public class BangDiemDAO {
+
+}
